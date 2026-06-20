d9a9641673b359276bb86411a4c984b246797909
Amine03824 <amine.boukraa@protonmail.com>
2026-06-20 23:59:51 +0200

    fix : split

--- split/run.js
+++ split/run.js
@@ -2,33 +2,25 @@
 const args = process.argv.slice(2);
 
 // Implement below:
-const splitComma = ",";
 let output = [];
 let string = "";
-console.log(args);
-
-for ( arg of args) {                    // Pour chaque élément du tableau
-console.log ("argument :" + arg);
-for (let i = 0 ; i <= arg.length; i++){ // Je parcoure les chars
-
-if (arg[i] === splitComma){             // Si l'élément contient une virgule 
-console.log("j'ai trouvé une virgule");
-output.push(string);                    // Je rajoute au tableau de stortie la string créée à partir des chars la précédant dans le tableau
-console.log("J'ai push dans le tableau output la string " + string);
-string = ""                             // Je remets à zéro la variable string
-
-} else if (arg === ""){
-output.push("");
-
-} else { 			       // Si l'élément est un texte normal
-string += arg[i];                      // Je l'ajoute à une variable "temporaire" string
-console.log("j'ai rajouté à la string la lettre " + arg[i]);
-
+for (arg of args){
+	for (i = 0; i < arg.length; i++){
+		if (arg[i] != ",") {
+			string += arg[i];
+			console.log(string);	
+		} else {
+			output.push(string);
+			string = "" ;
+		}
+	}
 }
 
-}
-}
-console.log(output);
 // Do not change:
-//console.log(output);
+console.log(output);
+
+
+
+
+
 
